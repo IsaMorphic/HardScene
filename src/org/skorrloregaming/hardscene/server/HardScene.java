831fca04d051f01b9ed50e9c2ba31f71030629db
SkorrloreGaming <skorrloregaming.dev@gmail.com>
2016-12-03 12:16:51 -0600

    Update to modern code

--- src/org/skorrloregaming/hardscene/server/HardScene.java
+++ src/org/skorrloregaming/hardscene/server/HardScene.java
@@ -18,108 +18,113 @@ import org.skorrloregaming.hardscene.server.impl.LoggerImpl;
 import org.skorrloregaming.hardscene.server.thread.HardScene_LoopThread;
 
 public class HardScene {
-	
+
 	public static boolean running = false;
 	public static HashMap<Integer, ClientImpl> clients = new HashMap<>();
 	public static ServerSocket server = null;
 	public static Config config = null;
 	public static boolean insecure = false;
-	
+
 	public static Properties bannedManager = null;
 	public static Properties opManager = null;
-	
+
 	public static String frameName = "HardScene";
-	
+
 	public static HardScene instance = null;
-	
+
 	public static boolean legacy = true;
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		instance = new HardScene();
 		instance.onEnable();
 	}
-	
-	public void onEnable(){
+
+	public void onEnable() {
 		opManager = new Properties(new File("hardscene_operators.properties"));
-    	bannedManager = new Properties(new File("hardscene_banned.properties"));
+		bannedManager = new Properties(new File("hardscene_banned.properties"));
 		startServer();
 	}
-	
-	public boolean startServer(){
-		if (running) return false;
-    	running = true;
+
+	public boolean startServer() {
+		if (running)
+			return false;
+		running = true;
 		try {
 			config = new Config();
 		} catch (IOException e1) {
 			e1.printStackTrace();
 			System.exit(-1);
 		}
-		System.out.println("Starting bind on port "+config.port+"..");
+		System.out.println("Starting bind on port " + config.port + "..");
 		try {
 			server = new ServerSocket(config.port);
 		} catch (Exception e) {
 			e.printStackTrace();
-			System.out.println("Failed to bind HardScene to port "+config.port+".");
+			System.out.println("Failed to bind HardScene to port " + config.port + ".");
 			running = false;
 			return false;
 		}
 		Thread acceptIncomingConnections = new Thread(new HardScene_LoopThread());
 		acceptIncomingConnections.start();
 		System.out.println("Server started, waiting for incoming connections..");
-	    Scanner scanner = new Scanner(System.in);
- 		while (running){
- 			System.out.print("> ");
- 		    String input = scanner.nextLine();
- 		    new CommandProcessEvent(input.split(" "), new LoggerImpl());
- 		}
- 	    scanner.close();
- 	    return true;
+		Scanner scanner = new Scanner(System.in);
+		while (running) {
+			System.out.print("> ");
+			String input = scanner.nextLine();
+			new CommandProcessEvent(input.split(" "), new LoggerImpl());
+		}
+		scanner.close();
+		return true;
 	}
-	
-	public static String trim(String str){
+
+	public static String trim(String str) {
 		String output = "";
 		boolean foundStart = false;
-		for (int i = 0; i < str.length(); i++){
-			if (!(str.charAt(i) == (char)' ')){
-				if (!foundStart) foundStart = true;
+		for (int i = 0; i < str.length(); i++) {
+			if (!(str.charAt(i) == (char) ' ')) {
+				if (!foundStart)
+					foundStart = true;
 				output += str.charAt(i);
-			}else{
-				if (foundStart){
+			} else {
+				if (foundStart) {
 					output += str.charAt(i);
 				}
 			}
 		}
 		return output;
 	}
-	
-	public static void log(String message){
-		if (config.log){
-			try{
-				String time = "["+new SimpleDateFormat("HH:mm:ss").format(new Date())+"]";
+
+	public static void log(String message) {
+		if (config.log) {
+			try {
+				String time = "[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "]";
 				String logMessage = '\n' + time + " " + trim(message);
 				File file = new File("hardscene_log.txt");
-	            if (!file.exists()) file.createNewFile();
-	            FileWriter writer = new FileWriter(file, true);
-	            BufferedWriter bufferedWriter = new BufferedWriter(writer);
-	            bufferedWriter.write(logMessage, 0, logMessage.length());
-	            bufferedWriter.close();
-			}catch (Exception ex){ex.printStackTrace();}
+				if (!file.exists())
+					file.createNewFile();
+				FileWriter writer = new FileWriter(file, true);
+				BufferedWriter bufferedWriter = new BufferedWriter(writer);
+				bufferedWriter.write(logMessage, 0, logMessage.length());
+				bufferedWriter.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
 		}
 	}
-	
-	public static void broadcast(String message, boolean direct) throws IOException{
+
+	public static void broadcast(String message, boolean direct) throws IOException {
 		log(message);
 		byte[] messageBytes = message.getBytes();
-		for (ClientImpl c : clients.values()){
-			if (direct){
+		for (ClientImpl c : clients.values()) {
+			if (direct) {
 				c.socket.getOutputStream().write(messageBytes, 0, messageBytes.length);
-			}else{
-				messageBytes[0] = (byte)' ';
-				messageBytes[1] = (byte)' ';
-				c.socket.getOutputStream().write(messageBytes, 0, messageBytes.length);	
+			} else {
+				messageBytes[0] = (byte) ' ';
+				messageBytes[1] = (byte) ' ';
+				c.socket.getOutputStream().write(messageBytes, 0, messageBytes.length);
 			}
 			c.socket.getOutputStream().flush();
 		}
 	}
-	
+
 }
--- src/org/skorrloregaming/hardscene/server/thread/HardScene_LoopThread.java
+++ src/org/skorrloregaming/hardscene/server/thread/HardScene_LoopThread.java
@@ -22,33 +22,6 @@ public class HardScene_LoopThread implements Runnable{
 				e.printStackTrace();
 				break;
 			}
-			// SEND THE HASH CODE : SECURITY FEATURE
-			try {
-				String[] hashBytesOrig = HardScene.config.hash.split("/");
-				byte[] hashBytes = new byte[4];
-				for (int i = 0; i < hashBytesOrig.length; i++){
-					hashBytes[i] = Byte.parseByte(hashBytesOrig[i]);
-				}
-				try{
-					socket.getOutputStream().write(hashBytes, 0, hashBytes.length);
-					socket.getOutputStream().flush();	
-				}catch (Exception ex){
-					ex.printStackTrace();
-				}
-			} catch (Exception ignored) {
-				System.out.println("Failed. The specified hash in the config is invalid, reverting..");
-				String[] hashBytesOrig = "8/0/2/7".split("/");
-				byte[] hashBytes = new byte[4];
-				for (int i = 0; i < hashBytesOrig.length; i++){
-					hashBytes[i] = Byte.parseByte(hashBytesOrig[i]);
-				}
-				try{
-					socket.getOutputStream().write(hashBytes, 0, hashBytes.length);
-					socket.getOutputStream().flush();	
-				}catch (Exception ex){
-					ex.printStackTrace();
-				}
-			}
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e1) {
@@ -68,27 +41,10 @@ public class HardScene_LoopThread implements Runnable{
 					socket.getInputStream().read(messageBytes, 0, messageBytes.length);
 					String name = new String(messageBytes, StandardCharsets.UTF_8);
 					boolean hasSupportedSymbols = true;
-					ClientImpl client = null;
-					try{
-						client = new ClientImpl(socket, random.nextInt(10000), name.split("\\$")[1]);
-					}catch (Exception ignored){
-						System.out.println(socket.getRemoteSocketAddress().toString().split(":")[0] + " is not using supported symbols.");
-						client = new ClientImpl(socket, random.nextInt(10000), name);
-						try {
-							new ClientDisconnectEvent(client, true);
-						} catch (Exception ig) {}
-						hasSupportedSymbols = false;
-					}
+					ClientImpl client = new ClientImpl(socket, random.nextInt(10000), " " + name.trim());
 					if (hasSupportedSymbols){
-						// GET THE CLIENT TOKEN : SECURITY FEATURE
-						String returnToken = messageBytes[0] + "/"+ messageBytes[1] + "/"+ messageBytes[2] + "/"+ messageBytes[3];
 						if (HardScene.clients.size() > HardScene.config.maxClients){
 							System.out.println(client.address +" has been denied access to connect due to the max clients threshold.");
-							try {
-								new ClientDisconnectEvent(client, true);
-							} catch (Exception ignored) {}
-						}else if (!returnToken.equals(HardScene.config.hash)){
-							System.out.println(client.address +" has been denied access to connect due to an invalid token : "+returnToken);
 							try {
 								new ClientDisconnectEvent(client, true);
 							} catch (Exception ignored) {}
